72958ebc557648f8582bf51d66245323c71d3a61
LS Dev <larasantanadev@gmail.com>
2025-05-28 00:52:26 -0300

    chore: reestrutura projeto e adiciona autenticação com dashboard

---
+++ src/lib/auth.ts
@@ -0,0 +1,36 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+
+import { db } from "@/db";
+import * as schema from "@/db/schema";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+    usePlural: true,
+    schema,
+  }),
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
+
+  // Define os modelos de usuário, sessão, conta e verificação para o banco de dados
+  user: {
+    modelName: "usersTable",
+  },
+  session: {
+    modelName: "sessionsTable",
+  },
+  account: {
+    modelName: "accountsTable",
+  },
+  verification: {
+    modelName: "verificationsTable",
+  },
+  emailAndPassword: {
+    enabled: true,
+  },
+});
